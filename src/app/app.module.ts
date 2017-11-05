3d8285877a3fa6733655b2a708b205ee9f280b57
moeabdol <mohd.a.saed@gmail.com>
2017-11-06 01:02:37 +0300

    Create pages.component and basic routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PagesComponent } from './components/pages/pages.component';
 
 import { PageService } from './services/page.service';
+
+const appRoutes: Routes = [
+  { path: ':page', component: PagesComponent },
+  { path: '', component: PagesComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     PageService
--- src/app/services/page.service.ts
+++ src/app/services/page.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class PageService {
   public pagesBS = new BehaviorSubject<string>(null);
 
-  constructor(private _http: Http) { }
+  constructor(private _http: HttpClient) { }
+
+  getPages() {
+    return this._http.get('http://localhost:3000/pages');
+  }
 }
