9c13d9459a8d5acedbca92e135beb0f3dc319a80
moeabdol <mohd.a.saed@gmail.com>
2017-11-07 20:47:28 +0300

    Implement admin-edit-page.component.html

--- src/app/components/admin-edit-page/admin-edit-page.component.ts
+++ src/app/components/admin-edit-page/admin-edit-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { PageService } from '../../services/page.service';
 
 @Component({
   selector: 'app-admin-edit-page',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-edit-page.component.scss']
 })
 export class AdminEditPageComponent implements OnInit {
+  public title: string;
+  public content: string;
+  public id: string;
+  public params: any;
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute,
+              private _pageService: PageService) { }
 
   ngOnInit() {
+    this._route.params.subscribe(params => {
+      this.params = params['id'];
+      this._pageService.getEditPage(this.params)
+        .subscribe(
+          page => {
+            this.title = page['title'];
+            this.content = page['content'];
+            this.id = page['id'];
+          },
+          err => {
+          }
+        );
+    });
   }
-
 }
